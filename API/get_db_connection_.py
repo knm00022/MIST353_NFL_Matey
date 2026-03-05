cdf3073c371ec59dcf6017c1ec659d01aa8057dc
knm00022 <knm00022@mix.wvu.edu>
2026-03-05 14:06:59 -0500

    Added two python functions

---
+++ API/get_db_connection_.py
@@ -0,0 +1,15 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
+load_dotenv()   
+def get_db_connection():
+    server = os.getenv('DB_SERVER')
+    database = os.getenv('DB_NAME')
+    username = os.getenv('DB_LOGIN')
+    password = os.getenv('DB_PASSWORD')
+
+    connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout = 30;" 
+    return pyodbc.connect(connection_string)
+
+
+    
