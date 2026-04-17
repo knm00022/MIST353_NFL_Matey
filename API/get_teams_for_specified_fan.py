3a6830174f70925caed0b289e95f1c9a49e4b3ec
knm00022 <knm00022@mix.wvu.edu>
2026-04-17 16:44:50 -0400

    errors

--- API/get_teams_for_specified_fan.py
+++ API/get_teams_for_specified_fan.py
@@ -1,18 +1,13 @@
 from get_db_connection import get_db_connection
-import pymssql
 
-def get_teams_for_specified_fan(
-        fan_id: int
-    ):
-    #with get_db_connection() as conn:
+def get_teams_for_specified_fan(fan_id: int):
     conn = get_db_connection()
     cursor = conn.cursor(as_dict=True)
-    #cursor.callproc("procGetTeamsForSpecifiedFan", (fan_id))
-    cursor.execute("exec procGetTeamsForSpecifiedFan %s", (fan_id))#for single param, use execute with exec procName param
+
+    cursor.execute("exec procGetTeamsForSpecifiedFan %s", (fan_id,))
     rows = cursor.fetchall()
     conn.close()
 
-    #Convert pymssql.Row objects to dicts
     results = [
         {
             "TeamName": row["TeamName"],
